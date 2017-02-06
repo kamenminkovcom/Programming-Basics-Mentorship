6c1b3127ed426284a73daf4a72225517e1c63515
Kamen Minkov <kamenminkov.com@abv.bg>
2017-02-06 11:48:15 +0200

    Add files via upload

---
+++ Loops/backToThePast.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class MainClass
+{
+    static void Main()
+    {
+        //https://judge.softuni.bg/Contests/Practice/Index/233#3
+        double money = double.Parse(Console.ReadLine());
+        int year = int.Parse(Console.ReadLine());
+        double neededMoney = 0;
+        for (int i = 1800; i <= year; i++)
+        {
+            if (i%2==0)
+            {
+                neededMoney += 12000;
+            }
+            else
+            {
+                double moneyForOddYear = 12000 + 50 * (18 + i - 1800);
+                neededMoney = neededMoney + moneyForOddYear; 
+            }
+        }
+
+        if (neededMoney <= money)
+        {
+            Console.WriteLine("Yes! He will live a carefree life and will have {0:f2} dollars left.", Math.Abs(money - neededMoney));
+        }
+        else
+        {
+            Console.WriteLine("He will need {0:f2} dollars to survive.", Math.Abs(money - neededMoney));
+        }
+    }
+}
+
---
+++ Loops/diviationWithoutRemainderWhileLoop.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        double p1 = 0;
+        double p2 = 0;
+        double p3 = 0;
+        int index = 1;
+        while(index<=n)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            if (number%2==0)
+            {
+                p1++;
+            }
+
+            if (number % 3 == 0)
+            {
+                p2++;
+            }
+            if (number % 4 == 0)
+            {
+                p3++;
+            }
+            index++;
+        }
+
+        Console.WriteLine("{0:f2}%", p1 / n * 100);
+        Console.WriteLine("{0:f2}%", p2 / n * 100);
+        Console.WriteLine("{0:f2}%", p3 / n * 100);
+    }
+}
+
