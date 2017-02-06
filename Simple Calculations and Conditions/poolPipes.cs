9937c56f09aecd1035b0e668920c8a7f3d167c1d
Kamen Minkov <kamenminkov.com@abv.bg>
2017-02-06 12:39:35 +0200

    Add files via upload

---
+++ Simple Calculations and Conditions/poolPipes.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class MianClass
+{
+    static void Main()
+    {
+        //https://judge.softuni.bg/Contests/Practice/Index/179#1
+        int V = int.Parse(Console.ReadLine());
+        int pipe1 = int.Parse(Console.ReadLine());
+        int pipe2 = int.Parse(Console.ReadLine());
+        double hours = double.Parse(Console.ReadLine());
+        double totalWater = (pipe1 + pipe2) * hours;
+        //Math.Truncate truncates the the digits after the decimal point e.g.: 14.9999 => 14
+        if (V>=totalWater)
+        {
+            double pipe1Percent = pipe1 * hours / totalWater * 100;
+            double pipe2Percent = pipe2 * hours / totalWater * 100;
+            Console.WriteLine($"The pool is {Math.Truncate((totalWater / V) * 100)}% full. Pipe 1: {Math.Truncate(pipe1Percent)}%. Pipe 2: {Math.Truncate(pipe2Percent)}%.");
+        }
+        else
+        {
+            Console.WriteLine($"For {hours} hours the pool overflows with {Math.Abs(V - totalWater)} liters.");
+        }
+    }
+}
+
---
+++ Simple Calculations and Conditions/poolPipesSecond.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //https://judge.softuni.bg/Contests/Practice/Index/179#1
+        int V = int.Parse(Console.ReadLine());
+        int P1 = int.Parse(Console.ReadLine());
+        int P2 = int.Parse(Console.ReadLine());
+        double H = double.Parse(Console.ReadLine());
+        double full = P1 * H + P2 * H;
+        //Math.Truncate truncates the the digits after the decimal point e.g.: 14.9999 => 14
+        if (full <= V)
+        {
+            Console.WriteLine("The pool is {0}% full. Pipe 1: {1}%. Pipe 2: {2}%.",
+                Math.Truncate(full / V * 100),
+                Math.Truncate(P1 * H / full * 100),
+                Math.Truncate(P2 * H / full * 100));
+
+        }
+        else
+        {
+            Console.WriteLine("For {0} hours the pool overflows with {1} liters.", H, full - V);
+        }
+    }
+}
