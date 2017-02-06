43a258478a3a7bdb8fedef2f112e7b11f10e070a
Kamen Minkov <kamenminkov.com@abv.bg>
2017-02-06 19:50:36 +0200

    Add files via upload

---
+++ Simple Calculations and Conditions/figureAreas.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+
+class MainClass
+{
+    static void Main()
+    {
+        string figure = Console.ReadLine();
+        double s = 0;
+        if (figure == "square")
+        {
+            double a = double.Parse(Console.ReadLine());
+            s = a*a;
+        }
+        else if (figure == "rectangle")
+        {
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            s = a*b;
+        }
+        else if (figure == "circle")
+        {
+            double r = double.Parse(Console.ReadLine());
+            s = r*r*Math.PI;   
+        }
+        else
+        {
+            double a = double.Parse(Console.ReadLine());
+            double h = double.Parse(Console.ReadLine());
+            s = a*h/2;
+            
+        }
+        Console.WriteLine(s);
+    }
+}
+
