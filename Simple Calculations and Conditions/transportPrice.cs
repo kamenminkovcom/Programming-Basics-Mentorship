493b0d0d45799f160be7156b54945d4664a1d541
Kamen Minkov <kamenminkov.com@abv.bg>
2017-02-06 12:56:20 +0200

    Add files via upload

---
+++ Simple Calculations and Conditions/transportPrice.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class MainClass
+{
+    static void Main()
+    {
+        //https://judge.softuni.bg/Contests/Practice/Index/169#1
+        int kilometers = int.Parse(Console.ReadLine());
+        string shift = Console.ReadLine();
+        double minPrice = 0;
+        if (kilometers < 20)
+        {
+            if (shift == "day")
+            {
+                minPrice = 0.7 + kilometers * 0.79;
+            }
+            else
+            {
+                minPrice = 0.7 + kilometers * 0.9;
+            }
+        }
+        else if (kilometers < 100)
+        {
+            minPrice = kilometers * 0.09;
+        }
+        else
+        {
+            minPrice = kilometers * 0.06;
+        }
+        Console.WriteLine(minPrice);
+    }
+}
+
