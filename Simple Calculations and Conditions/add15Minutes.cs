a78c4e7704f2a0cafa241b53c635b893152e90c0
Kamen Minkov <kamenminkov.com@abv.bg>
2017-02-08 21:29:13 +0200

    Add files via upload

---
+++ Simple Calculations and Conditions/add15Minutes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class MainClass
+{
+    static void Main()
+    {
+        string hours = Console.ReadLine();
+        string minutes = Console.ReadLine();
+        string time = hours + ":" + minutes;
+        var formatedTime = DateTime.ParseExact(time, "H:m", null).AddMinutes(15);
+        Console.WriteLine(formatedTime.ToString("H:mm"));
+    }
+}
+
